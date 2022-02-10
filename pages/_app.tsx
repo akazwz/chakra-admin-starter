1d8df4d5f004746e8f45c01e2c2afc3eb4ed7f18
akazwz <akazwz@pm.me>
2022-02-11 02:25:03 +0800

    add layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { RecoilRoot } from 'recoil'
+import { ChakraProvider } from '@chakra-ui/react'
+import chakraTheme from '../src/chakra-theme'
+import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp ({ Component, pageProps }: AppProps) {
+  return (
+    <RecoilRoot>
+      <ChakraProvider theme={chakraTheme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </RecoilRoot>
+  )
 }
 
 export default MyApp
